14cf3cb7881cbe6740c11b49d1d1b9b0a4764fb6
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-02-19 18:24:21 -0800

    update1

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//OBJECTS
+var metallica = {
+    Artist: "Metallica",
+    Album: "Master of Puppets (1986)",
+    Song: "Welcome Home(Sanitarium)"
+}
+var pantera = {
+    Artist: "Pantera",
+    Album: "Cowboys From Hell (1990)",
+    Song: "Domination"
+}
+var aliceinchains = {
+    Artist: "Alice in Chains",
+    Album: "Dirt (1992)",
+    Song: "Would?"
+}
+var queen = {
+    Artist: "Queen",
+    Album: "Innuendo (1991)",
+    Song: "Innuendo"
+}
+
+//VARIABLES
+var allowedGuesses;
+var incorrectGuesses;
+var correctGuesses;
+var songs = [
+    "sanitarium",
+    "domination",
+    "would",
+    "innuendo"
+];
+
+//RESET GAME
+
+
+//FUNCTIONS
+
+
+
+
