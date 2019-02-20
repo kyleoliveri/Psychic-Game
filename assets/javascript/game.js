fd82cd292b7c79161f21b1922abd4668b281fdad
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-02-19 21:41:11 -0800

    updated2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,21 +21,73 @@ var queen = {
 }
 
 //VARIABLES
-var allowedGuesses;
-var incorrectGuesses;
-var correctGuesses;
+var incorrectGuesses = [];
+var wordGuess = [];
 var songs = [
     "sanitarium",
     "domination",
     "would",
     "innuendo"
 ];
+var currentWordIndex;
+var remainingGuesses = 0;
+var wins = 0;
+var losses = 0;
+var gameStart = false;
+var gameFinish = false;
+const maxAttempts = 12;
 
-//RESET GAME
+//MUSIC
+var metallicaSong = new Audio("../assets/songs/metallica-WelcomeHome(Sanitarium).mp3");
+var panteraSong = new Audio("../assets/songs/pantera-Domination.mp3");
+var aliceInChainsSong = new Audio("../assets/songs/aliceInChains-Would.mp3");
+var queenSong = new Audio("../assets/songs/queen-Innuendo.mp3");
 
+// //WIN COUNT
+// document.getElementById("winsh4").innerHTML = "Wins: " + wins;
 
-//FUNCTIONS
+// //LOSS COUNT
+// document.getElementById("lossh4").innerHTML = "Losses: " + losses;
 
+function gameReset() {
+    
+    remainingGuesses = maxAttempts;
 
+    currentWordIndex = Math.floor(Math.random() * (songs.length));
 
+    incorrectGuesses = [];
+    wordGuess = [];
+
+    //HIDE IMAGES & WIN/LOSS BANNERS AT START
+    document.getElementById("puppetz").src = "";
+    document.getElementById("innuendo").src = "";
+    document.getElementById("cowboysFromHell").src = "";
+    document.getElementById("aliceInChains").src = "";
+    document.getElementById("youLose").src = "";
+
+    for (var i = 0; i < songs[currentWordIndex].length; i++) {
+        wordGuess.push("_");
+    }
+
+    document.getElementById("winner1").style.cssText = "display: none";
+    document.getElementById("winner2").style.cssText = "display: none";
+    document.getElementById("loser1").style.cssText = "display: none";
+    document.getElementById("loser2").style.cssText = "display: none";
+
+    updateDisplay();
+};
+
+function updateDisplay() {
+
+    document.getElementById("songName").innerText = "";
+    for (var i = 0; i < wordGuess.length; i++) {
+        document.getElementById("songName").innerText += wordGuess[i];
+    }
+    document.getElementById("guessesRemaining").innerText = remainingGuesses;    
+    document.getElementById("lettersGuessed").innerText = incorrectGuesses;
+    if(remainingGuesses <= 0) {
+        document.getElementById("loser1").style.cssText = "display: block";
+    }
+
+};
 
