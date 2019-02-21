91d1fe330789460c260bfe8218f99df7b1fe7cff
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-02-20 22:51:00 -0800

    Updated3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,6 +21,7 @@ var queen = {
 }
 
 //VARIABLES
+var wordGuessText = "";
 var incorrectGuesses = [];
 var wordGuess = [];
 var songs = [
@@ -38,17 +39,12 @@ var gameFinish = false;
 const maxAttempts = 12;
 
 //MUSIC
-var metallicaSong = new Audio("../assets/songs/metallica-WelcomeHome(Sanitarium).mp3");
-var panteraSong = new Audio("../assets/songs/pantera-Domination.mp3");
-var aliceInChainsSong = new Audio("../assets/songs/aliceInChains-Would.mp3");
-var queenSong = new Audio("../assets/songs/queen-Innuendo.mp3");
+var metallicaSong = new Audio("/assets/songs/metallica-WelcomeHome(Sanitarium).mp3");
+var panteraSong = new Audio("/assets/songs/pantera-Domination.mp3");
+var aliceInChainsSong = new Audio("/assets/songs/aliceInChains-Would.mp3");
+var queenSong = new Audio("/assets/songs/queen-Innuendo.mp3");
 
-// //WIN COUNT
-// document.getElementById("winsh4").innerHTML = "Wins: " + wins;
-
-// //LOSS COUNT
-// document.getElementById("lossh4").innerHTML = "Losses: " + losses;
-
+//DISPLAYS MAX ATTEMPS, CLEARS ARRAYS & HIDES IMAGES
 function gameReset() {
     
     remainingGuesses = maxAttempts;
@@ -77,17 +73,76 @@ function gameReset() {
     updateDisplay();
 };
 
+//UPDATES DISPLAY WITH WINS/LOSSES & GUESSES
 function updateDisplay() {
+    
+    document.getElementById("winsh4").innerText = ("Wins: " + wins);
+    document.getElementById("lossh4").innerText = ("Losses: " + losses);
 
-    document.getElementById("songName").innerText = "";
     for (var i = 0; i < wordGuess.length; i++) {
-        document.getElementById("songName").innerText += wordGuess[i];
+        wordGuessText += wordGuess[i];
     }
+    
+    document.getElementById("songName").innerText = wordGuessText;
     document.getElementById("guessesRemaining").innerText = remainingGuesses;    
     document.getElementById("lettersGuessed").innerText = incorrectGuesses;
-    if(remainingGuesses <= 0) {
-        document.getElementById("loser1").style.cssText = "display: block";
-    }
-
+    
 };
 
+function evaluateGuess(letter) {
+    var positions = [];
+
+    for (var i = 0; i < songs[currentWordIndex].length; i++) {
+        if(songs[currentWordIndex][i] === letter) {
+            positions.push(i);
+        }
+    }
+};
+
+//CHECK FOR WINS
+function checkForWin() {
+    if (wordGuess.indexOf("_") === -1) {
+        document.getElementById("winner1").style.cssText = "display: block";
+        document.getElementById("winner2").style.cssText = "display: block";
+        wins++;
+        metallicaSong.play();
+        gameFinish = true;
+    }
+};
+
+//CHECK FOR LOSS
+function checkForLoss() {
+
+    if (remainingGuesses <= 0) {
+    document.getElementById("loser1").style.cssText = "display: block";
+    document.getElementById("loser2").style.cssText = "display: block";
+    document.getElementById("youLose").src = "assets/images/youLose.jpg";
+    gameFinish = true;
+    }
+};
+
+//MAKES GUESS
+function makeGuess(letter) {
+    if (remainingGuesses > 0) {
+        if (incorrectGuesses.indexOf(letter) === -1) {
+            incorrectGuesses.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+};
+
+//EVENT KEY
+document.onkeydown = function(event) {
+    if (gameFinish) {
+        gameReset();
+        gameFinish = false;
+    }
+    else {
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            makeGuess(event.key.toUpperCase());
+            updateDisplay();
+            checkForWin();
+            checkForLoss();
+        }
+    }
+};
